ddfaa4b65bc7134607d1d4531cfc1c4dfb29f097
Felipe Ferreira <flpferreira1999@gmail.com>
2024-07-30 14:31:32 -0300

    ajustando placeholder

--- src/components/CadastrarPaciente.tsx
+++ src/components/CadastrarPaciente.tsx
@@ -78,7 +78,7 @@ export default function CadastrarPaciente() {
       type: "text",
       name: "cpf",
       className: "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 focus: outline-none mb-5",
-      placeholder: "CPF",
+      placeholder: "CPF (Apenas dígitos)",
       required: true,
     },
     {
