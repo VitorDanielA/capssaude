ecec9b0fa8c571d03b933db44d263ec2071e64a4
Felipe Ferreira <flpferreira1999@gmail.com>
2024-08-31 18:53:17 -0300

    adicionando tipo em login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
         setError('');
         setLoading(true);
